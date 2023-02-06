828a2d78322cc7f071072ab4747c6cc68c7a8745
Sagi, Cohen <sagispacecohen@gmail.com>
2023-02-06 22:51:42 +0200

    main.py is created

---
+++ main.py
@@ -0,0 +1,56 @@
+# x = 5
+# y = 8
+#
+# if x > y:
+#     print('BIG')
+# else:
+#     print('small')
+#
+#
+# for numbers in range(5):
+#     print(numbers)
+#
+# seasons = 1
+# if seasons == 1:
+#     print('summer')
+# elif seasons == 2:
+#     print('winter')
+# elif seasons == 3:
+#     print('fall')
+# elif seasons == 4:
+#     print('winter')
+# else:
+#     print('seasons must include 1-4 numbers')
+
+# age = 31
+# lastN = 'C'
+# currency = 3.38
+# flyAboard = True
+# apartment = 23
+# print(age, lastN, currency, flyAboard, apartment)
+# sum = age + currency
+# print(sum)
+#
+# phoneNumber = input('Enter ur Phone number')
+# print(f'phone number {phoneNumber}')
+#
+# def print_hello():
+#     print("hello")
+#
+# def calcule():
+#     print(5 + 3)
+#
+# print_hello()
+# calcule()
+
+
+
+try:
+    a = 1/0
+except:
+    print("devine by 0 is not iilegal")
+
+try:
+    x = 1
+finally:
+    print('finally')
